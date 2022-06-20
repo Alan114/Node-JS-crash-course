836e266a2d35aee1657eb686f9e2ed5718ac4f4a
Alan Pecenkovic <alanquad@yahoo.com>
2022-06-20 16:46:57 +0200

    saving documents to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const dbURI =
   "mongodb+srv://alan:test@nodejscourse.xiiubco.mongodb.net/?retryWrites=true&w=majority";
@@ -18,6 +19,22 @@ app.set("view engine", "ejs");
 // middleware & static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.get("/", (req, res) => {
   const blogs = [
