942be6eb0c1e10f6123dd14ad790265df2b08b87
Alan Pecenkovic <alanquad@yahoo.com>
2022-06-21 10:37:25 +0200

    get all blogs and get a single blog

--- app.js
+++ app.js
@@ -23,13 +23,29 @@ app.use(morgan("dev"));
 // mongoose and mongo sandbox routes
 app.get("/add-blog", (req, res) => {
   const blog = new Blog({
-    title: "new blog",
+    title: "new blog 2",
     snippet: "about my new blog",
     body: "more about my new blog",
   });
 
   blog
     .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("62b0870eaf19fe7cfaf21643")
     .then((result) => {
       res.send(result);
     })
